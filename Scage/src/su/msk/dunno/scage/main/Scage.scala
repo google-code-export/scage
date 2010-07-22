2871b17ac42883ac3685942c6f9cdb175960396b
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-07-22 14:17:51 +0000

    Failed commit: Default

--- Scage/src/su/msk/dunno/scage/main/Scage.scala
+++ Scage/src/su/msk/dunno/scage/main/Scage.scala
@@ -1,11 +1,11 @@
 package su.msk.dunno.scage.main
 
 import java.util.Properties
-import java.io.FileInputStream
 import su.msk.dunno.scage.handlers.controller.Controller
 import su.msk.dunno.scage.prototypes.{THandler}
 import su.msk.dunno.scage.handlers.{AI, Physics, Idler, Renderer}
 import org.apache.log4j.Logger
+import java.io.{FileNotFoundException, FileInputStream}
 
 object Scage {
   private val log = Logger.getLogger(Scage.getClass);
@@ -14,15 +14,36 @@ object Scage {
   private val properties:Properties = {
     if(properties == null) {
       val p:Properties = new Properties()
-      p.load(new FileInputStream("options.txt"))
+      try{p.load(new FileInputStream("options.txt"))}
+      catch {
+        case ex:FileNotFoundException =>
+          log.debug("failed to load properties: options.txt not found")
+          System.exit(0)
+      }
       p
     }
     else properties
   }
-  def getProperty(key:String):String = properties.getProperty(key)
-  def getIntProperty(key:String):Int = Integer.valueOf(properties.getProperty(key)).intValue
-  def getFloatProperty(key:String):Float = java.lang.Float.valueOf(properties.getProperty(key)).floatValue
-  def getBooleanProperty(key:String):Boolean = properties.getProperty(key).equalsIgnoreCase("yes")
+  def getProperty(key:String):String = {
+    val s = properties.getProperty(key)
+    log.debug("read property "+key+": "+s)
+    s
+  }
+  def getIntProperty(key:String):Int = {
+    val i = Integer.valueOf(properties.getProperty(key)).intValue
+    log.debug("read property "+key+": "+i)
+    i
+  }
+  def getFloatProperty(key:String):Float = {
+    val f = java.lang.Float.valueOf(properties.getProperty(key)).floatValue
+    log.debug("read property "+key+": "+f)
+    f
+  }
+  def getBooleanProperty(key:String):Boolean = {
+    val b= properties.getProperty(key).equalsIgnoreCase("yes")
+    log.debug("read property "+key+": "+b)
+    b
+  }
 
   private var handlers = List[THandler]()
   def getHandlers() = handlers
@@ -38,7 +59,6 @@ object Scage {
   
   private var isRunning = true
   def start() = {
-	log.debug("starting game...")
     Idler
     isRunning = true
     handlers.foreach(h => h.initSequence)
@@ -47,13 +67,20 @@ object Scage {
   def stop() = {isRunning = false}
 
   private def run():Unit = {
-	if(isRunning) {
-	  handlers.foreach(h => h.actionSequence)
-      run
-	}
-	else {
-		handlers.foreach(h => h.exitSequence)
-		log.debug("game was stopped")
-	}
+    if(isRunning) {
+      handlers.foreach(h => h.actionSequence)
+        run
+    }
+    else {
+      handlers.foreach(h => h.exitSequence)
+      log.debug("app was stopped")
+    }
+  }
+
+  def main(args:Array[String]):Unit = {
+    val app_classname = getProperty("app")
+    log.debug("starting app "+app_classname)
+    Class.forName(app_classname).getField("MODULE$").get(null)
+    start
   }
 }

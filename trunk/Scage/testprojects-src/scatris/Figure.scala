1b8f12592a04022863da33c43928966bef1cb507
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-10-03 20:10:18 +0000

    failed

--- trunk/Scage/testprojects-src/scatris/Figure.scala
+++ trunk/Scage/testprojects-src/scatris/Figure.scala
@@ -4,35 +4,47 @@ import su.msk.dunno.scage.handlers.AI
 import su.msk.dunno.scage.handlers.controller.Controller
 import org.lwjgl.input.Keyboard
 import su.msk.dunno.scage.support.tracer.StandardTracer
-import su.msk.dunno.scage.support.ScageLibrary
+import su.msk.dunno.scage.support.{Vec, ScageLibrary}
 
 abstract class Figure extends ScageLibrary {
   val name:String
   val points:List[Point]
 
-  private def excludedTraces = points.map(point => point.trace)
-  def canMoveDown = {
-    points.foldLeft(true)((can_move_down, point) => can_move_down && point.canMoveDown(excludedTraces))
+  private def canMove(dir: (Point) => Boolean) = {
+    points.filter(point => point.isActive) match {
+      case Nil => false
+      case active_points =>
+        active_points.foldLeft(true)((can_move, point) => can_move && dir(point))
+    }
   }
+
+  private def excludedTraces = points.map(point => point.trace)
 
   private var last_move_time = System.currentTimeMillis
   private var is_acceleration = false
-  private def movePeriod = if(!is_acceleration) 300 else 50
+  private def movePeriod = if(!is_acceleration) 1000 else 50
   private def isNextMove = System.currentTimeMillis - last_move_time > movePeriod
+
+  private val down = Vec(0, -StandardTracer.h_y)
+  def canMoveDown:Boolean = canMove(point => point.canMove(excludedTraces, down))
   AI.registerAI(() => {
-    if(isNextMove) {
-      if(canMoveDown) points.sortWith((p1, p2) => p1.coord.y < p2.coord.y).foreach(point => point.moveDown)
+    if(isNextMove && canMoveDown) {
+      points.foreach(point => point.move(excludedTraces, down))
       last_move_time = System.currentTimeMillis
     }
   })
 
-  Controller.addKeyListener(Keyboard.KEY_LEFT, 1000, () => {
-    if(canMoveDown) points.sortWith((p1, p2) => p1.coord.x < p2.coord.x).foreach(point => point.moveLeft)
+  private val left = Vec(-StandardTracer.h_x, 0)
+  private def canMoveLeft = canMove(point => point.canMove(excludedTraces, left))
+  Controller.addKeyListener(Keyboard.KEY_LEFT, 100, () => {
+    if(canMoveDown && canMoveLeft) points.foreach(point => point.move(excludedTraces, left))
   })
 
-  Controller.addKeyListener(Keyboard.KEY_RIGHT, 1000, () => {
-    if(canMoveDown) points.sortWith((p1, p2) => p1.coord.x > p2.coord.x).foreach(point => point.moveRight)
+  private val right = Vec(StandardTracer.h_x, 0)
+  private def canMoveRight = canMove(point => point.canMove(excludedTraces, right))
+  Controller.addKeyListener(Keyboard.KEY_RIGHT, 100, () => {
+    if(canMoveDown && canMoveRight) points.foreach(point => point.move(excludedTraces, right))
   })
 
-  Controller.addKeyListener(Keyboard.KEY_DOWN, 1000, () => is_acceleration = true, () => is_acceleration = false)
+  Controller.addKeyListener(Keyboard.KEY_DOWN, 500, () => is_acceleration = true, () => is_acceleration = false)
 }

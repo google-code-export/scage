71bfd2da3692adb74c7e790ba41bc9ce834dee0c
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-10-08 14:34:40 +0000

    tracer almost finished

--- trunk/Scage/testprojects-src/colors2/Colors2Test.scala
+++ trunk/Scage/testprojects-src/colors2/Colors2Test.scala
@@ -7,6 +7,21 @@ import org.lwjgl.input.Keyboard
 import su.msk.dunno.scage2.support.messages.Message
 
 object Colors2Test extends Application with ScageLibrary {
+  val COLOR_MARQUEE_TEXT = new Color(248,212,168)
+  val COLOR_TOOLTIP_TEXT = new Color(248,212,168)
+  val COLOR_MENU_TEXT = new Color(248,212,168)
+  val COLOR_PL_TEXT_NORM = new Color(248,212,168)
+  val COLOR_PL_TEXT_PLAYING = new Color(247,148,29)
+  val COLOR_PL_TEXT_ERROR = new Color(132,80,16)
+  val COLOR_PL_TEXT_SELECTED = new Color(248,212,168)
+  val COLOR_PL_BK_SELECTED = new Color(14,70,27)
+  val COLOR_PL_FOCUS_RECT = new Color(247,148,29)
+  val COLOR_DVR_TEXT_NORM = new Color(248,212,168)
+  val COLOR_DVR_TEXT_RECORDING = new Color(247,148,29)
+  val COLOR_DVR_TEXT_ERROR = new Color(255,0,0)
+  val COLOR_DVR_TEXT_SELECTED = new Color(248,212,168)
+  val COLOR_DVR_BK_SELECTED = new Color(14,70,27)
+
   val fields = this.getClass.getDeclaredFields
   val colors = fields.map(f => {
     f.setAccessible(true)
@@ -16,13 +31,13 @@ object Colors2Test extends Application with ScageLibrary {
     }
   })
 
-  var color_num = 3
+  var color_num = 1
   val main_screen = new Screen("Main Screen") {
     renderer.addInterfaceElement(() => if(color_num >= 0 && color_num < fields.length) {
       Message.print(fields(color_num).getName, 400, 300, if("BLACK".equalsIgnoreCase(fields(color_num).getName)) WHITE else BLACK)
       try {renderer.setBackground(colors(color_num))}
       catch {
-        case ex:java.lang.ClassCastException =>
+        case ex:java.lang.Exception =>
       }
     })
 
@@ -30,7 +45,7 @@ object Colors2Test extends Application with ScageLibrary {
       def nextColorNumInc() {
         if(color_num < fields.length - 1) color_num += 1
         else color_num = 0
-        if(!WHITE.equals(colors(color_num)) || "WHITE".equalsIgnoreCase(fields(color_num).getName)) color_num
+        if(colors(color_num) != null && (!WHITE.equals(colors(color_num)) || "WHITE".equalsIgnoreCase(fields(color_num).getName))) color_num
         else nextColorNumInc
       }
       nextColorNumInc

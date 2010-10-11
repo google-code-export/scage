2e198d7363671e12efc5a0c68ebed53c998a43bd
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-10-11 04:59:42 +0000

    failed

--- Scage/scage-src/su/msk/dunno/scage/handlers/net/NetServer.scala
+++ Scage/scage-src/su/msk/dunno/scage/handlers/net/NetServer.scala
@@ -1,10 +1,12 @@
 package su.msk.dunno.scage.handlers.net
 
 import su.msk.dunno.scage.main.Scage
-import java.net.ServerSocket
-import org.json.JSONObject
 import su.msk.dunno.scage.prototypes.Handler
 import su.msk.dunno.scage.support.ScageProperties
+import java.io.{BufferedReader, InputStreamReader, OutputStreamWriter, PrintWriter}
+import org.apache.log4j.Logger
+import java.net.{Socket, SocketException, ServerSocket}
+import org.json.{JSONException, JSONObject}
 
 object NetServer extends Handler {
   val port = ScageProperties.intProperty("port", 9800)
@@ -64,4 +66,54 @@ object NetServer extends Handler {
     client_handlers.foreach(client => client.send(new JSONObject().put("quit", "")))
     client_handlers.foreach(client => client.disconnect)
   }
+}
+
+class ClientHandler(val id:Int, val socket:Socket) {
+  private val log = Logger.getLogger(this.getClass)
+
+  private val out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream))
+  private val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+
+  private var cd:JSONObject = new JSONObject
+  def incomingData = {
+    has_new_data = false
+    cd
+  }
+
+  private var has_new_data = false
+  def hasNewIncomingData = has_new_data
+
+  def send(data:JSONObject) = {
+    out.println(data)
+    out.flush
+  }
+
+  def disconnect = {
+    socket.close
+    log.debug("client #"+id+" was disconnected")
+  }
+
+  private val check_timeout = ScageProperties.intProperty("check_timeout", 0)
+  private var last_answer_time = System.currentTimeMillis
+  def isOnline = check_timeout == 0 || System.currentTimeMillis - last_answer_time < check_timeout
+
+  new Thread(new Runnable { // receive data from client
+    def run():Unit = {
+      while(Scage.isRunning) {
+        if(in.ready) {
+          last_answer_time = System.currentTimeMillis
+          val message = try{in.readLine}
+          catch {
+            case e:SocketException => return
+          }
+          cd = try{new JSONObject(message)}
+          catch {
+            case e:JSONException => cd.put("raw", message)
+          }
+          if(cd.length > 0) has_new_data = true
+        }
+        Thread.sleep(10)
+       }
+    }
+  }).start
 }
--- Scage/scage2-src/su/msk/dunno/scage2/support/ScageProperties.scala
+++ Scage/scage2-src/su/msk/dunno/scage2/support/ScageProperties.scala
@@ -10,10 +10,10 @@ object ScageProperties {
   private val properties:Properties = {
     if(properties == null) {
       val p:Properties = new Properties()
-      try{p.load(new FileInputStream("options.txt"))}
+      try{p.load(new FileInputStream("scage-properties.txt"))}
       catch {
         case ex:FileNotFoundException =>
-          log.error("failed to load properties: options.txt not found")
+          log.debug("failed to load properties: scage-properties.txt not found")
           System.exit(0)
       }
       p
@@ -22,22 +22,22 @@ object ScageProperties {
   }
   def get(key:String):String = {
     val s = properties.getProperty(key)
-    log.info("read property "+key+": "+s)
+    log.debug("read property "+key+": "+s)
     s
   }
   def getInt(key:String):Int = {
     val i = Integer.valueOf(properties.getProperty(key)).intValue
-    log.info("read property "+key+": "+i)
+    log.debug("read property "+key+": "+i)
     i
   }
   def getFloat(key:String):Float = {
     val f = java.lang.Float.valueOf(properties.getProperty(key)).floatValue
-    log.info("read property "+key+": "+f)
+    log.debug("read property "+key+": "+f)
     f
   }
   def getBoolean(key:String):Boolean = {
     val b = properties.getProperty(key).equalsIgnoreCase("yes")
-    log.info("read property "+key+": "+b)
+    log.debug("read property "+key+": "+b)
     b
   }
 }

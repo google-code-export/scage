983fdd4297482ccb25eca915bcb5b99435e615b2
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-10-11 15:38:34 +0000

    tracer almost finished

--- Scage/testprojects-src/scagetest/TestProject.scala
+++ Scage/testprojects-src/scagetest/TestProject.scala
@@ -3,6 +3,20 @@ package scagetest
 import su.msk.dunno.scage.support.ScageProperties
 
 object TestProject extends Application {
-  val p = ScageProperties.intProperty("name", 5)
-  println(p)
+  //val list = List(9,8,7,6,5,4,3,2,1,0)
+  //val range = 1 to 3
+  //foreachpair(1 to 3)(println(_))
+  println(makeThree(1 to 3).length)
+
+  def foreachpair(range:Range)(doIt:((Any, Any)) => Unit) = {
+    range.foldLeft(List[(Int, Int)]())((pairs, number) =>
+      (range zip List().padTo(range.length, number)).toList ::: pairs)
+         .foreach(doIt(_))
+  }
+
+  def makeThree(range:Range) = {
+    val two = range.foldLeft(List[(Int, Int)]())((pairs, number) => (range zip List().padTo(range.length, number)).toList ::: pairs)
+    range.foldLeft(List[((Int, Int), Int)]())((triples, number) => (two zip List().padTo(range.length*range.length, number)).toList ::: triples)
+         .map(three => (three._1._1, three._1._2, three._2))
+  }
 }

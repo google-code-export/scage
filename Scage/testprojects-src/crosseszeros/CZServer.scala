083a7baac0ac104c86f8516e8be507d6e330024f
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-10-20 15:48:44 +0000

    some improvements in net handlers

---
+++ Scage/testprojects-src/crosseszeros/CZServer.scala
@@ -0,0 +1,31 @@
+package crosseszeros
+
+import su.msk.dunno.scage.support.ScageLibrary._
+import su.msk.dunno.scage.handlers.net.NetServer
+import su.msk.dunno.scage.handlers.AI
+import java.lang.String
+
+object CZServer {
+  /*case class MyString(val s:String)
+  properties = "cz-properties.txt"
+
+  NetServer.greetings = "CrossesZeros, v 0.1"
+
+  val board = Array.ofDim[Int](5,5)
+
+  var is_crosses_move= true
+  val logEntry = "(\d+) (\d+)".r
+  AI.registerAI(() => {
+    if(NetServer.clients.length == 2) {
+      if(is_crosses_move) {
+        val cross = NetServer.clients(0)
+        if(cross.hasNewIncomingData) {
+          val data = cross.incomingData
+          if(data.has("raw")) data.getString("raw") {
+
+          }
+        }
+      }
+    }
+  })*/
+}

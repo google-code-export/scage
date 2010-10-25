1a687c027c6ed6efc8a90a2b0abcac89c7bb456a
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-10-25 15:31:38 +0000

    tests

--- Scage/testprojects-src/helloworld/HelloWorld.scala
+++ Scage/testprojects-src/helloworld/HelloWorld.scala
@@ -3,14 +3,18 @@ package helloworld
 import su.msk.dunno.scage.support.ScageLibrary._
 import su.msk.dunno.scage.support.messages.Message
 import su.msk.dunno.scage.handlers.{Idler, Renderer}
-import su.msk.dunno.scage.Scage
+import su.msk.dunno.scage.support.Vec
 
 object HelloWorld {
-  Renderer.addInterfaceElement(() => Message.print("Hello World! "+Idler.fps, 400, 300))
-
-  Scage.action(5000) {
-    println("ttt")
+  var coord = Vec(0, 0)
+  val center = Vec(400,300)
+  var x = Vec(100, 0)
+  action(100) {
+    x = x.rotateDeg(1)
+    coord = center + x
   }
+
+  Renderer.addInterfaceElement(() => Message.print("Hello World! "+Idler.fps, coord))
 
   def main(args:Array[String]):Unit = start
 }

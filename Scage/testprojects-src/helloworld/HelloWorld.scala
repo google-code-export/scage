9ce6f39ba3ed16084950c8c40607a688e7bef85c
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-12-03 15:46:07 +0000

    added cyrillic unicode droid and fixed all errors at last!

--- Scage/testprojects-src/helloworld/HelloWorld.scala
+++ Scage/testprojects-src/helloworld/HelloWorld.scala
@@ -3,7 +3,8 @@ package helloworld
 import su.msk.dunno.screens.ScageScreen
 import su.msk.dunno.screens.prototypes.Renderable
 import su.msk.dunno.screens.handlers.Renderer
-import su.msk.dunno.scage.support.messages.{MyFont, Message}
+import su.msk.dunno.scage.support.Vec
+import su.msk.dunno.scage.support.messages.{TrueTypeFont, MyFont, Message}
 
 object HelloWorld extends Application {
   //private val uFont = new MyFont
@@ -11,9 +12,9 @@ object HelloWorld extends Application {
   val myScreen = new ScageScreen("My Screen", is_main = true, "blame-properties.txt") {
     addRender(new Renderable {
       override def interface = {
-        //uFont.uFont.drawString(Renderer.width/2, Renderer.height/2, "Hello World!", org.newdawn.slick.Color.white)
-         Message.print("Привет Мир!", Renderer.width/2, Renderer.height/2)
-        Message.print("Овлоло, овлоло, вы все быдло и хуйло!", Renderer.width/2, Renderer.height/2 - 20)
+         Message.print("Привет, Мир!", Renderer.width/2, Renderer.height/2)
+         //Message.print("Hello, World!", Renderer.width/2, Renderer.height/2-20)
+         Renderer.drawCircle(Vec(Renderer.width/2, Renderer.height/2), 5)
       }
     })
 

da5e718bebceedd32ab6d411cfd849d4941a5d70
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-12-09 05:48:37 +0000

    screens are not object again

--- Scage/src-blamer/su/msk/dunno/blame/screens/InventoryScreen.scala
+++ Scage/src-blamer/su/msk/dunno/blame/screens/InventoryScreen.scala
@@ -5,32 +5,23 @@ import su.msk.dunno.screens.prototypes.Renderable
 import org.lwjgl.input.Keyboard
 import su.msk.dunno.blame.prototypes.Inventory
 
-object InventoryScreen extends ScageScreen("InventoryScreen") {  
-  private var _inventory:Inventory = null
-  def show(inventory:Inventory) {
-    _inventory = inventory
-    super.run
-  }
-
-  def pickItem(inventory:Inventory) {
-    _inventory = inventory
-    super.run
-    _inventory.selectedItem
+class InventoryScreen(val inventory:Inventory) extends ScageScreen("InventoryScreen") {
+  def pickItem = {
+    run
+    inventory.selectedItem
   }
 
   addRender(new Renderable {
     override def interface = _inventory.show      
   })
   
-  keyListener(Keyboard.KEY_1, onKeyDown = _inventory.itemSelector = 1)
-  keyListener(Keyboard.KEY_2, onKeyDown = _inventory.itemSelector = 2)
-  keyListener(Keyboard.KEY_3, onKeyDown = _inventory.itemSelector = 3)
-  keyListener(Keyboard.KEY_4, onKeyDown = _inventory.itemSelector = 4)
-  keyListener(Keyboard.KEY_5, onKeyDown = _inventory.itemSelector = 5)
+  keyListener(Keyboard.KEY_1, onKeyDown = inventory.itemSelector = 1)
+  keyListener(Keyboard.KEY_2, onKeyDown = inventory.itemSelector = 2)
+  keyListener(Keyboard.KEY_3, onKeyDown = inventory.itemSelector = 3)
+  keyListener(Keyboard.KEY_4, onKeyDown = inventory.itemSelector = 4)
+  keyListener(Keyboard.KEY_5, onKeyDown = inventory.itemSelector = 5)
   keyListener(Keyboard.KEY_ESCAPE, onKeyDown = {
     if(_inventory.itemSelector != -1) _inventory.itemSelector = -1
     else stop
   })
-
-  override def run = {}
 }

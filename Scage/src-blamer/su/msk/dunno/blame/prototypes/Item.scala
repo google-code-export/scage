853d4228372af5ef4bbe4c2e8187e3f7ce8d0972
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-12-20 16:09:31 +0000

    items are FieldObjects now

--- Scage/src-blamer/su/msk/dunno/blame/prototypes/Item.scala
+++ Scage/src-blamer/su/msk/dunno/blame/prototypes/Item.scala
@@ -4,30 +4,24 @@ import su.msk.dunno.screens.support.tracer.State
 import su.msk.dunno.blame.field.{FieldObject, FieldTracer}
 import su.msk.dunno.scage.support.{Vec, ScageColor}
 
-class Item(val name:String, val description:String, val symbol:Int, val color:ScageColor) 
-extends HaveStats {
+class Item(val name:String, val description:String, val symbol:Int, val color:ScageColor)
+extends FieldObject with HaveStats {
+  setStat("item")
   setStat("name", name)
   setStat("description", description)
 
   private val _point = Vec(0,0)
   private var _trace = -1
-  private val fieldObject = new FieldObject {
-    def getCoord = FieldTracer.pointCenter(_point)
-    def getSymbol = symbol
-    def getColor = color
-    def isTransparent = true
-    def isPassable = true
 
-    def getState = stats
-    def changeState(s:State) = {}
-  }
+  override def getPoint = _point
+  def getCoord = FieldTracer.pointCenter(_point)
+  def getSymbol = symbol
+  def getColor = color
+  def isTransparent = true
+  def isPassable = true
 
-  def drop(point:Vec) = {
-    _point is point
-    _trace = FieldTracer.addTrace(fieldObject)
-  }
-
-  def pickUp = {
-    FieldTracer.removeTraceFromPoint(_trace, _point)
+  def getState = stats
+  def changeState(s:State) = {
+    if(s.contains("location")) _point is s.getVec("location")
   }
 }

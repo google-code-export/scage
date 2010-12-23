8804c6bf3dc345538911f6e81aa105f9b65cd883
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2010-12-23 14:57:03 +0000

    I will rather provide this build for me through dropbox =)

--- trunk/Scage/builds/projects/blamer-v01-linux/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-            java -Djava.library.path=native -DLWJGL_DISABLE_XRANDR=true -cp lib/commons-logging-1.1.1.jar:lib/jinput.jar:lib/log4j-1.2.15.jar:lib/lwjgl.jar:lib/lwjgl_util.jar:lib/phys2d-060408.jar:lib/scala-library.jar:lib/slick.jar:lib/json.jar:lib/rlforj-nosources-0.2.jar:lib/scage-v03.jar:lib/blamer-v01.jar su.msk.dunno.blame.screens.Blamer
-        	

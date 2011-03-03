e6d77c2997aae054c67d83ea3ae29b96d7116dc8
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-03-03 06:48:34 +0000

    shit doesn't work!

--- Scarcanoid/src/main/scala/su/msk/dunno/scar/Physics.scala
+++ Scarcanoid/src/main/scala/su/msk/dunno/scar/Physics.scala
@@ -5,6 +5,7 @@ import _root_.net.phys2d.raw.{Body, World}
 import net.phys2d.math.Vector2f
 import _root_.net.phys2d.raw.strategies.QuadSpaceStrategy
 import su.msk.dunno.scage.screens.prototypes.{ScageAction}
+import su.msk.dunno.scage.single.support.Vec
 
 object Physics {
   val dt = property("dt", 5)
@@ -12,16 +13,21 @@ object Physics {
   val friction = property("friction", 0.0f)
   val world = new World(new Vector2f(0.0f, gravity), 10, new QuadSpaceStrategy(20,5));
   world.enableRestingBodyDetection(0.01f, 0.000001f, 0.01f)
-  world.setDamping(10)
 
-  def addBody(b:Body) = if(!world.getBodies.contains(b)) {
-    //b.setFriction(friction)
-    world.add(b)
+  def addBody(body:Body) = {
+    if(!world.getBodies.contains(body)) {
+      body.setRestitution(1.0f)
+      body.setFriction(friction)
+      world.add(body)
+    }
   }
 
   Scaranoid.addAction(new ScageAction {
     override def action = {
-      for(i <- 1 to dt) world.step()
+      for(i <- 1 to dt) {
+        print("*")
+        world.step()
+      }
     }
   })
 }

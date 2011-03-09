5ae160570e9d819776395c9fc6133e4f8772c61a
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-03-09 06:45:22 +0000

    returned to closures for actions, inits, exits, etc. Try to test performance one more time

--- trunk/ScageMaven/src/main/scala/su/msk/dunno/scage/screens/ScageScreen.scala
+++ trunk/ScageMaven/src/main/scala/su/msk/dunno/scage/screens/ScageScreen.scala
@@ -19,17 +19,30 @@ class ScageScreen(val screen_name:String, val is_main_screen:Boolean = false, pr
   if(is_main_screen) log.info("starting "+screen_name+"...")
   if(!"".equals(properties)) ScageProperties.properties = properties
   
-  private var handlers:List[ScageAction] = Nil
+  /*private var handlers:List[ScageAction] = Nil
   def addAction(handler:ScageAction, period:Long = 0) =
     if(period > 0) handlers = new ActionWaiter(period, handler) :: handlers
-    else handlers = handler :: handlers
+    else handlers = handler :: handlers*/
+
+  private var inits:List[() => Unit] = Nil
+  def init(init_func: => Unit) = if(is_running) inits else inits = (() => init_func) :: inits
+
+  private var actions:List[() => Unit] = Nil
+  def action(action_func: => Unit) = actions = (() => action_func) :: actions
+  def action(action_period:Long)(action_func: => Unit) =
+    actions = new ActionWaiter(action_period, action_func).doAction :: actions
+
+  private var exits:List[() => Unit] = Nil
+  def exit(exit_func: => Unit) = exits = (() => exit_func) :: exits
 
   val controller = new Controller
   def keyListener(key: => Int, repeatTime: => Long = 0, onKeyDown: => Any, onKeyUp: => Any = {}) =
     controller.keyListener(key, repeatTime, onKeyDown, onKeyUp)
 
   val renderer = if(is_main_screen) new Renderer(main_screen = this) else new Renderer
-  def addRender(render:ScageRender) = renderer.addRender(render)
+  /*def addRender(render:ScageRender) = renderer.addRender(render)*/
+  def render(render_func: => Unit) = renderer.render(render_func)
+  def interface(interface_func: => Unit) = renderer.interface(interface_func)
 
   def scale = renderer.scale
   def scale_= (value:Float) = renderer.scale = value
@@ -48,15 +61,15 @@ class ScageScreen(val screen_name:String, val is_main_screen:Boolean = false, pr
 
   private var is_running = false
   def isRunning = is_running
-  def init = handlers.foreach(handler => handler.init)
-  def exit = handlers.foreach(handler => handler.exit)
+  def init = /*handlers.foreach(handler => handler.init)*/ inits.foreach(init_func => init_func())
+  def exit = /*handlers.foreach(handler => handler.exit)*/ exits.foreach(exit_func => exit_func())
   def run = {
     if(!is_main_screen) log.info("starting "+screen_name+"...")
     init
     is_running = true
     while(is_running && !ScageScreen.isAllScreensStop) {
       controller.checkControls
-      if(!on_pause) handlers.foreach(handler => handler.action)
+      if(!on_pause) /*handlers.foreach(handler => handler.action)*/ actions.foreach(action_func => action_func())
       renderer.render
     }
     exit
@@ -68,12 +81,23 @@ class ScageScreen(val screen_name:String, val is_main_screen:Boolean = false, pr
     else is_running = false 
   }
 
-  private[ScageScreen] class ActionWaiter(val period:Long, private val action_handler:ScageAction) extends ScageAction {
+  /*private[ScageScreen] class ActionWaiter(val period:Long, private val action_handler:ScageAction) extends ScageAction {
     private var last_action_time:Long = 0
 
     override def action {
       if(System.currentTimeMillis - last_action_time > period) {
         action_handler.action
+        last_action_time = System.currentTimeMillis
+      }
+    }
+  }*/
+
+  private[ScageScreen] class ActionWaiter(period:Long, action_func: => Unit) {
+    private var last_action_time:Long = 0
+
+    def doAction = () => {
+      if(System.currentTimeMillis - last_action_time > period) {
+        action_func
         last_action_time = System.currentTimeMillis
       }
     }

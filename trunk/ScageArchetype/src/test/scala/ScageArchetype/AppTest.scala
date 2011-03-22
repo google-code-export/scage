387ad739c0ef17f2916c007c7f02fe0ee7ea2ce0
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-03-22 05:58:36 +0000

    archetype for scage projects, initial commit

---
+++ trunk/ScageArchetype/src/test/scala/ScageArchetype/AppTest.scala
@@ -0,0 +1,32 @@
+package ScageArchetype;
+
+import junit.framework._;
+import Assert._;
+
+object AppTest {
+    def suite: Test = {
+        val suite = new TestSuite(classOf[AppTest]);
+        suite
+    }
+
+    def main(args : Array[String]) {
+        junit.textui.TestRunner.run(suite);
+    }
+}
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest extends TestCase("app") {
+
+    /**
+     * Rigourous Tests :-)
+     */
+    def testOK() = {
+      ScageArchetype.App
+      assertTrue(true)
+    };
+    //def testKO() = assertTrue(false);
+    
+
+}

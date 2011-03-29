64a25d2bbf779083988ae22e50e99ffce4c22e7e
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-03-29 20:36:31 +0000

    changes in game to satisfy with new scage version

--- Scarcanoid/src/main/scala/su/msk/dunno/scar/Scaranoid.scala
+++ Scarcanoid/src/main/scala/su/msk/dunno/scar/Scaranoid.scala
@@ -22,16 +22,25 @@ object Scaranoid extends PhysicsScreen(
     count = 0
   }
 
-  new StaticLine(Vec(30,  10),   Vec(30,  470)) -> this
-  new StaticLine(Vec(30,  470),  Vec(630, 470)) -> this
-  new StaticLine(Vec(630, 470),  Vec(630, 10)) -> this
-  new StaticLine(Vec(630, 10),   Vec(30,  10)) {
+  this --> new StaticLine(Vec(30,  10),   Vec(30,  470))
+  this --> new StaticLine(Vec(30,  470),  Vec(630, 470))
+  this --> new StaticLine(Vec(630, 470),  Vec(630, 10))
+  this --> new StaticLine(Vec(630, 10),   Vec(30,  10)) {
+    init {
+      this.prepare
+    }
+
     action {
       if(isTouching) pause
     }
-  } -> this
+  }
 
+  val physics_screen = this
   class TargetBox(leftup_coord:Vec) extends StaticBox(leftup_coord, 40, 40) {
+    init {
+      physics_screen --> this
+    }
+
     action {
       if(isTouching) {
         count += 1
@@ -41,22 +50,22 @@ object Scaranoid extends PhysicsScreen(
     }
   }
 
-  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 460)) -> this
-  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 415)) -> this
-  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 370)) -> this
+  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 460))
+  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 415))
+  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 370))
 
-  val player_platform = new StaticBox(Vec(width/2,25), 50, 10) {
+  val player_platform = this --> new StaticBox(Vec(width/2,25), 50, 10) {
     init {
       coord = Vec(width/2,25)
     }
-  } -> this
+  }
 
   key(KEY_LEFT,  10, onKeyDown = if(!onPause && player_platform.coord.x > 60) player_platform.move(Vec(-3, 0)))
   key(KEY_RIGHT, 10, onKeyDown = if(!onPause && player_platform.coord.x < 600) player_platform.move(Vec(3, 0)))
 
   val ball_radius = property("ball.radius", 5)
   val ball_speed = property("ball.speed", 25)
-  new DynaBall(Vec(width/2, height/2), ball_radius) {
+  this --> new DynaBall(Vec(width/2, height/2), ball_radius) {
     action {
       if(velocity.norma < ball_speed-1)
         velocity = velocity.n * ball_speed
@@ -68,7 +77,7 @@ object Scaranoid extends PhysicsScreen(
       coord = Vec(width/2, height/2)
       velocity = new Vec(-ball_speed, -ball_speed)
     }
-  } -> this
+  }
 
   interface {
     if(onPause) {

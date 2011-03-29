1284391b640eeee7b24a7419de4ec921f176ad31
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-03-29 07:30:14 +0000

    version 1.3 - using scage 0.4.0 now (moved physics stuff to engine)

--- Scarcanoid/src/main/scala/su/msk/dunno/scar/Scaranoid.scala
+++ Scarcanoid/src/main/scala/su/msk/dunno/scar/Scaranoid.scala
@@ -1,112 +1,96 @@
 package su.msk.dunno.scar
 
+import su.msk.dunno.scage.screens.physics._
+import su.msk.dunno.scage.screens.physics.objects._
+
 import su.msk.dunno.scage.screens.ScageScreen
 import su.msk.dunno.scage.single.support.Vec
 import su.msk.dunno.scage.single.support.ScageColors._
 import su.msk.dunno.scage.screens.handlers.Renderer._
 import net.phys2d.math.Vector2f
-import org.lwjgl.input.Keyboard
+import org.lwjgl.input.Keyboard._
 import su.msk.dunno.scage.single.support.ScageProperties._
-import su.msk.dunno.scage.screens.prototypes.{ScageAction, ScageRender}
 import su.msk.dunno.scage.single.support.messages.ScageMessage._
-import su.msk.dunno.scage.screens.handlers.Renderer._
 
-object Scaranoid extends ScageScreen(
+object Scaranoid extends PhysicsScreen(
   screen_name = "Scaranoid",
   is_main_screen = true,
   properties = "scaranoid-properties.txt"
 ) {
   private var count = 0
-  /*addAction(new ScageAction {
-    override def init = count = 0
-  })*/
   init {
     count = 0
   }
 
-  new StaticLine(Vec(30,  10),   Vec(30,  470))
-  new StaticLine(Vec(30,  470),  Vec(630, 470))
-  new StaticLine(Vec(630, 470),  Vec(630, 10))
+  new StaticLine(Vec(30,  10),   Vec(30,  470)) -> this
+  new StaticLine(Vec(30,  470),  Vec(630, 470)) -> this
+  new StaticLine(Vec(630, 470),  Vec(630, 10)) -> this
   new StaticLine(Vec(630, 10),   Vec(30,  10)) {
-    /*addAction(new ScageAction {*/
-      /*override def */action/* =*/ {
-        if(isTouching) pause
-      }
-    /*})*/
-  }
+    action {
+      if(isTouching) pause
+    }
+  } -> this
 
   class TargetBox(leftup_coord:Vec) extends StaticBox(leftup_coord, 40, 40) {
-    /*addAction(new ScageAction {
-      override def */action/* =*/ {
-        if(isActive && isTouching) {
-          count += 1
-          if(count >= 39) pause
-          isActive = false
-        }
+    action {
+      if(isTouching) {
+        count += 1
+        if(count >= 39) pause
+        isActive = false
       }
-    /*})*/
+    }
   }
-  private var boxes:List[TargetBox] = Nil
-  for(i <- 0 to 12) boxes = new TargetBox(Vec(35 + i*45, 460)) :: boxes
-  for(i <- 0 to 12) boxes = new TargetBox(Vec(35 + i*45, 415)) :: boxes
-  for(i <- 0 to 12) boxes = new TargetBox(Vec(35 + i*45, 370)) :: boxes
+
+  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 460)) -> this
+  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 415)) -> this
+  for(i <- 0 to 12) new TargetBox(Vec(35 + i*45, 370)) -> this
 
   val player_platform = new StaticBox(Vec(width/2,25), 50, 10) {
-    /*addAction(new ScageAction {
-      override def */init/* =*/ {
-        coord = Vec(width/2, 25)
-      }
-    /*})*/
-  }
-  keyListener(Keyboard.KEY_LEFT,  10, onKeyDown = if(!onPause && player_platform.coord.x > 60) player_platform.move(Vec(-3, 0)))
-  keyListener(Keyboard.KEY_RIGHT, 10, onKeyDown = if(!onPause && player_platform.coord.x < 600) player_platform.move(Vec(3, 0)))
+    init {
+      coord = Vec(width/2,25)
+    }
+  } -> this
+
+  keyListener(KEY_LEFT,  10, onKeyDown = if(!onPause && player_platform.coord.x > 60) player_platform.move(Vec(-3, 0)))
+  keyListener(KEY_RIGHT, 10, onKeyDown = if(!onPause && player_platform.coord.x < 600) player_platform.move(Vec(3, 0)))
 
   val ball_radius = property("ball.radius", 5)
   val ball_speed = property("ball.speed", 25)
-  val ball = new DynaBall(Vec(width/2, height/2), ball_radius) {
-    /*addAction(new ScageAction {
-      override def */action/* =*/ {
-        if(velocity.norma < ball_speed-1)
-          velocity = velocity.n * ball_speed
-        else if(math.abs(velocity.y) < 1)
-          velocity = Vec(velocity.x, 10*math.signum(velocity.y))
-      }
-    /*})*/
-
-    /*addAction(new ScageAction {
-      override def */init/* =*/ {
-        coord = Vec(width/2, height/2)
-        velocity = new Vec(-ball_speed, -ball_speed)
-      }
-    /*})*/
-  }
-
-  /*addRender(new ScageRender {
-    override def */interface/* =*/ {
-      if(onPause) {
-        if(count < 39) print(xml("game.lose"), width/2, height/2, WHITE)
-        else print(xml("game.win"), width/2, height/2, WHITE)
-        print(xml("game.playagain"), width/2, height/2-20, WHITE)
-      }
-      print(count, 5, height-20, WHITE)
-      print(fps, 5, height-40, WHITE)
-      //print(ball.velocity, 10, height-40, WHITE)
+  new DynaBall(Vec(width/2, height/2), ball_radius) {
+    action {
+      if(velocity.norma < ball_speed-1)
+        velocity = velocity.n * ball_speed
+      else if(math.abs(velocity.y) < 1)
+        velocity = Vec(velocity.x, 10*math.signum(velocity.y))
     }
-  /*})*/
-  keyListener(Keyboard.KEY_Y, onKeyDown = if(onPause) {
+
+    init {
+      coord = Vec(width/2, height/2)
+      velocity = new Vec(-ball_speed, -ball_speed)
+    }
+  } -> this
+
+  interface {
+    if(onPause) {
+      if(count < 39) print(xml("game.lose"), width/2, height/2, WHITE)
+      else print(xml("game.win"), width/2, height/2, WHITE)
+      print(xml("game.playagain"), width/2, height/2-20, WHITE)
+    }
+    print(count, 5, height-20, WHITE)
+    print(fps, 5, height-40, WHITE)
+  }
+  keyListener(KEY_Y, onKeyDown = if(onPause) {
     init
     pauseOff
   })
-  keyListener(Keyboard.KEY_N, onKeyDown = if(onPause) stop)
+  keyListener(KEY_N, onKeyDown = if(onPause) stop)
 
   new ScageScreen("Help Screen") {
-    keyListener(Keyboard.KEY_SPACE, onKeyDown = stop)
+    keyListener(KEY_SPACE, onKeyDown = stop)
 
-    /*addRender(new ScageRender {
-      override def */interface/* =*/ {
-        print(xml("helpscreen.helpmessage"), 10, height-20, WHITE)
-      }
-    /*})*/
+    interface {
+      print(xml("helpscreen.helpmessage"), 10, height-20, WHITE)
+    }
   }.run
 
   def main(args:Array[String]):Unit = run

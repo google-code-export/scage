e624c156a590f3bc65db7df39b480bbc657130bf
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-04-11 05:37:31 +0000

    continue to implement new tracer

--- ScageMaven/src/test/scala/su/msk/dunno/scage/test/ScageTest.scala
+++ ScageMaven/src/test/scala/su/msk/dunno/scage/test/ScageTest.scala
@@ -36,35 +36,24 @@ class ScageTest extends TestCase("app") {
       new ScageScreen("Hello World", is_main_screen = true, properties = "scagetest-properties.txt") {
         val tracer = new Tracer[Trace](0,640,0,480,32,24)
 
-        val coord = Vec(width/2, height/2)
-        val point = Vec(16,12)
-        val trace_id = tracer.addTrace(new Trace() {
-          def getPoint = tracer.point(coord)
+        val trace = tracer.addTrace(Vec(16,12), new Trace() {
           def getState = new State
           def changeState(changer:Trace, state:State) {}
         })
 
-        key(KEY_UP, 100, onKeyDown = {
-          tracer.updateLocation(trace_id, tracer.point(coord is coord + Vec(0,1)))
-        })
-        key(KEY_DOWN, 100, onKeyDown = {
-          tracer.updateLocation(trace_id, tracer.point(coord is coord + Vec(0,-1)))
-        })
-        key(KEY_RIGHT, 100, onKeyDown = {
-          tracer.updateLocation(trace_id, tracer.point(coord is coord + Vec(1,0)))
-        })
-        key(KEY_LEFT, 100, onKeyDown = {
-          tracer.updateLocation(trace_id, tracer.point(coord is coord + Vec(-1,0)))
-        })
+        key(KEY_UP,    100, onKeyDown = tracer.move(trace, Vec(0,1)))
+        key(KEY_DOWN,  100, onKeyDown = tracer.move(trace, Vec(0,-1)))
+        key(KEY_RIGHT, 100, onKeyDown = tracer.move(trace, Vec(1,0)))
+        key(KEY_LEFT,  100, onKeyDown = tracer.move(trace, Vec(-1,0)))
 
         backgroundColor = WHITE
         interface {
           print(xml("hello.world"), width/2, height/2, BLACK)
-          print(point, width/2, height/2-20, BLACK)
+          print(trace.point, width/2, height/2-20, BLACK)
         }
         render {
           Renderer.color = BLACK
-          Renderer.drawCircle(coord, 10)
+          Renderer.drawCircle(tracer.pointCenter(trace.point), 10)
         }
       }.run
       assertTrue(true)

2851e0a6ea97c57d445c5db70dc57e76ab389f15
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-04-25 22:29:52 +0000

    refactoring of ScageScreen, fixed bug with init_func not adding to list if is_running = true

--- trunk/ScageMaven/src/main/scala/su/msk/dunno/scage/screens/ScageScreen.scala
+++ trunk/ScageMaven/src/main/scala/su/msk/dunno/scage/screens/ScageScreen.scala
@@ -1,15 +1,15 @@
 package su.msk.dunno.scage.screens
 
 import handlers.controller.Controller
-import handlers.{Idler, Renderer}
+import handlers.Renderer
 import org.apache.log4j.Logger
-import su.msk.dunno.scage.single.support.{Vec}
+import su.msk.dunno.scage.single.support.Vec
 import su.msk.dunno.scage.single.support.ScageProperties
 
 object ScageScreen {
   private var isAllScreensStop = false
   def isAppRunning = !isAllScreensStop
-  def allStop = isAllScreensStop = true
+  def allStop() {isAllScreensStop = true}
 }
 
 class ScageScreen(val screen_name:String, val is_main_screen:Boolean = false, properties:String = "") {
@@ -24,71 +24,75 @@ class ScageScreen(val screen_name:String, val is_main_screen:Boolean = false, pr
     else handlers = handler :: handlers*/
 
   private var inits:List[() => Unit] = Nil
-  def init(init_func: => Unit) = if(is_running) init_func else inits = (() => init_func) :: inits
+  def init(init_func: => Unit) {
+    inits = (() => init_func) :: inits
+    if(is_running) init_func
+  }
 
   private var actions:List[() => Unit] = Nil
-  def action(action_func: => Unit) = actions = (() => action_func) :: actions
-  def action(action_period:Long)(action_func: => Unit) =
-    actions = new ActionWaiter(action_period, action_func).doAction :: actions
+  def action(action_func: => Unit) {actions = (() => action_func) :: actions}
+  /*def action(action_period:Long)(action_func: => Unit) =
+    actions = new ActionWaiter(action_period, action_func).doAction :: actions*/
 
   private var exits:List[() => Unit] = Nil
-  def exit(exit_func: => Unit) = exits = (() => exit_func) :: exits
+  def exit(exit_func: => Unit) {exits = (() => exit_func) :: exits}
 
   val controller = new Controller
-  def key(key: => Int, repeatTime: => Long = 0, onKeyDown: => Any, onKeyUp: => Any = {}) =
+  def key(key: => Int, repeatTime: => Long = 0, onKeyDown: => Any, onKeyUp: => Any = {}) {
     controller.key(key, repeatTime, onKeyDown, onKeyUp)
+  }
 
   val renderer = new Renderer
   /*def addRender(render:ScageRender) = renderer.addRender(render)*/
-  def render(render_func: => Unit) = renderer.render(render_func)
-  def interface(interface_func: => Unit) = renderer.interface(interface_func)
+  def render(render_func: => Unit) {renderer.render(render_func)}
+  def interface(interface_func: => Unit) {renderer.interface(interface_func)}
 
   def scale = renderer.scale
-  def scale_= (value:Float) = renderer.scale = value
+  def scale_= (value:Float) {renderer.scale = value}
   
   def windowCenter = renderer.windowCenter
-  def windowCenter_= (coord: => Vec) = renderer.windowCenter = coord
+  def windowCenter_= (coord: => Vec) {renderer.windowCenter = coord}
 
   def center = renderer.center
-  def center_= (coord: => Vec) = renderer.center = coord
+  def center_= (coord: => Vec) {renderer.center = coord}
 
   private var on_pause = false
   def onPause = on_pause
-  def switchPause = on_pause = !on_pause
-  def pause = on_pause = true
-  def pauseOff = on_pause = false
+  def switchPause() {on_pause = !on_pause}
+  def pause() {on_pause = true}
+  def pauseOff() {on_pause = false}
 
   private var is_running = false
   def isRunning = is_running
-  def init = {
+  def init() {
     /*handlers.foreach(handler => handler.init)*/
     log.info(screen_name+": init")
     inits.foreach(init_func => init_func())
   }
-  def exit = {
+  def exit() {
     /*handlers.foreach(handler => handler.exit)*/
     log.info(screen_name+": exit")
     exits.foreach(exit_func => exit_func())
   }
-  def run = {
+  def run() {
     if(!is_main_screen) log.info("starting screen "+screen_name+"...")
-    init
+    init()
     is_running = true
     log.info(screen_name+": run")
     while(is_running && !ScageScreen.isAllScreensStop) {
       controller.checkControls
       if(!on_pause) /*handlers.foreach(handler => handler.action)*/ actions.foreach(action_func => action_func())
-      renderer.render
+      renderer.render()
     }
-    exit
+    exit()
     log.info(screen_name+" was stopped")
     if(is_main_screen) {
-      renderer.exitRender
+      renderer.exitRender()
       System.exit(0)
     }
   }
-  def stop = {
-    if(is_main_screen) ScageScreen.allStop
+  def stop() {
+    if(is_main_screen) ScageScreen.allStop()
     else is_running = false 
   }
 
@@ -103,14 +107,14 @@ class ScageScreen(val screen_name:String, val is_main_screen:Boolean = false, pr
     }
   }*/
 
-  private[ScageScreen] class ActionWaiter(period:Long, action_func: => Unit) {
+  /*private[ScageScreen] class ActionWaiter(period:Long, action_func: => Unit) {
     private var last_action_time:Long = 0
 
-    def doAction = () => {
+    def doAction() {
       if(System.currentTimeMillis - last_action_time > period) {
         action_func
         last_action_time = System.currentTimeMillis
       }
     }
-  }
+  }*/
 }

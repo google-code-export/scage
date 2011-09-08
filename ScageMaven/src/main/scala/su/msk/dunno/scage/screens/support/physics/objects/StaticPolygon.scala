c9483caab7b85585702f2734c2e81831942702b4
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-09-08 14:49:56 +0000

    added misterious restitution  property (what is this, seriously??) to the other physical objects

--- ScageMaven/src/main/scala/su/msk/dunno/scage/screens/support/physics/objects/StaticPolygon.scala
+++ ScageMaven/src/main/scala/su/msk/dunno/scage/screens/support/physics/objects/StaticPolygon.scala
@@ -5,6 +5,7 @@ import su.msk.dunno.scage.screens.support.physics.Physical
 import net.phys2d.math.{ROVector2f, Vector2f}
 import net.phys2d.raw.shapes.Polygon
 import net.phys2d.raw.StaticBody
+import su.msk.dunno.scage.single.support.ScageProperties._
 
 class StaticPolygon(val vertices:Vec*) extends Physical {
   val polygon_vertices = for {
@@ -13,6 +14,7 @@ class StaticPolygon(val vertices:Vec*) extends Physical {
   } yield new Vector2f(new_vertice.x, new_vertice.y)
   val polygon = new Polygon(polygon_vertices.toArray)
   val body = new StaticBody("StaticPolygon", polygon)
+  if(property("physics.restitution", false)) body.setRestitution(1.0f)
   body.setPosition(vertices(0).x, vertices(0).y)
 
   private val vertices_array = vertices.toArray

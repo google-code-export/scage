b10817952a81e776addd8042e8345f516977b170
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2011-12-26 05:58:31 +0000

    started xmlInterface

--- ScageMaven/src/main/scala/net/scage/support/messages/ScageXML.scala
+++ ScageMaven/src/main/scala/net/scage/support/messages/ScageXML.scala
@@ -7,7 +7,8 @@ import xml.XML
 import org.newdawn.slick.util.ResourceLoader
 
 class ScageXML(val lang:String          = property("strings.lang", "en"),
-               val messages_base:String = property("strings.base", "resources/strings/" +stringProperty("app.name").toLowerCase+"_strings")
+               val messages_base:String = property("strings.base", "resources/strings/" +stringProperty("app.name").toLowerCase+"_strings"),
+               val interfaces_file:String = property("interfaces.file", "resources/"+stringProperty("app.name").toLowerCase+"_interfaces.xml")
 ) {
   private val log = Logger(this.getClass.getName)
   
@@ -29,36 +30,85 @@ class ScageXML(val lang:String          = property("strings.lang", "en"),
   }
 
   def xml(message_code:String, parameters:Any*):String = {
-    val xml_message = if(xml_messages.contains(message_code)) xml_messages(message_code)
+    if(message_code == "") ""
     else {
-      log.warn("failed to find string with code "+message_code)
-      //val warning_string = xmlOrDefault("error.nomessage","No message provided under the code ?", message_code)
-      xml_messages += (message_code -> /*warning_string*/message_code)
-      /*warning_string*/message_code
+      xml_messages.get(message_code) match {
+        case Some(message) => mergeMessage(message, parameters:_*)
+        case None => {
+          log.warn("failed to find string with code "+message_code)
+          xml_messages += (message_code -> message_code)
+          message_code
+        }
+      }
     }
-    mergeMessage(xml_message, parameters:_*)
   }
 
   def xmlOrDefault(message_code:String, parameters:Any*):String = {
-    if(xml_messages.contains(message_code)) mergeMessage(xml_messages(message_code), parameters.tail:_*)
-    else {
-      if(parameters.size > 0) {
+    xml_messages.get(message_code) match {
+      case Some(message) => mergeMessage(message, parameters.tail:_*)
+      case None if parameters.size > 0 => {
         log.info("default value for string code "+message_code+" is "+{
           if("" == parameters.head) "empty string" else parameters.head
         })
         xml_messages += (message_code -> parameters.head.toString)
         mergeMessage(parameters.head.toString, parameters.tail:_*)
-      } else {
-        //xmlOrDefault("error.nomessage","No message provided under the code ?", message_code)
+      }
+      case _ => {
         log.warn("failed to find default message for the code "+message_code)
         xml_messages += (message_code -> message_code)
         message_code
       }
     }
   }
+  
+  private def placeholdersAmount(message_id:String) = {
+    xml_messages.get(message_id) match {
+      case Some(message) => message.foldLeft(0)((sum, char) => sum + (if(char == '?') 1 else 0))
+      case None => 0
+    }
+  }
+
+  private val xml_interfaces = XML.load(ResourceLoader.getResourceAsStream(interfaces_file)) match {
+    case <interfaces>{interfaces_list @ _*}</interfaces> => {
+      HashMap((for {
+        interface @ <interface>{rows_list @ _*}</interface> <- interfaces_list
+        interface_id = (interface \ "@id").text
+        if interface_id != ""
+      } yield {
+        var overall_placeholder_position = 0
+        val messages = (for {
+          row @ <row>{_*}</row> <- rows_list
+          message_id = (row \ "@message_id").text
+          placeholders_in_row = placeholdersAmount(message_id)
+        } yield {
+          val to_yield = (message_id, placeholders_in_row, overall_placeholder_position)
+          overall_placeholder_position += placeholders_in_row
+          to_yield
+        }).toArray
+        (interface_id, messages)
+      }):_*)
+    }
+    case _ => HashMap[String, Array[(String, Int, Int)]]()
+  }
+  
+  def xmlInterface(interface_id:String, parameters:Any*):Array[String] = {
+    xml_interfaces.get(interface_id) match {
+      case Some(interface) => {
+        ((for {
+          (message_id, params_from, params_take) <- interface
+        } yield xml(message_id, (parameters.drop(params_from).take(params_take)):_*))).toArray
+      }
+      case None => {
+        log.warn("failed to find interface with id "+interface_id)
+        xml_interfaces += (interface_id -> Array((interface_id, 0, 0)))
+        Array(interface_id)
+      }
+    }  
+  }
 }
 
 object ScageXML extends ScageXML(
-  lang          = property("strings.lang", "en"),
-  messages_base = property("strings.base", "resources/strings/" +stringProperty("app.name").toLowerCase+"_strings")
+  lang            = property("strings.lang", "en"),
+  messages_base   = property("strings.base", "resources/strings/" +stringProperty("app.name").toLowerCase+"_strings"),
+  interfaces_file = property("interfaces.file", "resources/"+stringProperty("app.name").toLowerCase+"_interfaces.xml")
 )

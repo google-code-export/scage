abd39ec0f35b342d787aa44eb03277586bf37231
dunnolol.b <dunnolol.b@db5196b7-4cae-dc8f-5050-34a90979ceea>
2012-01-16 05:59:13 +0000

    * refactoring for properties in ScageXML * started to implement ScageApp trait (entry point to the program)

--- ScageMaven/src/main/scala/net/scage/ScageScreen.scala
+++ ScageMaven/src/main/scala/net/scage/ScageScreen.scala
@@ -4,14 +4,14 @@ import handlers.controller2.{ScageController, MultiController, SingleController}
 import handlers.Renderer
 import com.weiglewilczek.slf4s.Logger
 
-abstract class Screen(unit_name:String = "Scage App", is_main_unit:Boolean = false, properties:String = "")
-extends Scage(unit_name, is_main_unit, properties) with Renderer with ScageController {
+abstract class Screen(unit_name:String = "Scage Screen")
+extends Scage(unit_name, false, "") with Renderer with ScageController {
   private val log = Logger(this.getClass.getName)
 
-  // I could override del operations instead to not delete action operations from here (checlControls(), render() etc),
+  // I could override del operations instead to not delete action operations from here (checkControls(), render() etc),
   // or I could set new operation type - some kind of 'important' ops...
   override def run() {
-    if(!is_main_unit) log.info("starting unit "+unit_name+"...")
+    log.info("starting unit "+unit_name+"...")
     init()
     is_running = true
     log.info(unit_name+": run")
@@ -26,15 +26,39 @@ extends Scage(unit_name, is_main_unit, properties) with Renderer with ScageContr
     exit()
     dispose()
     log.info(unit_name+" was stopped")
-    if(is_main_unit/* && !Scage.isAppRunning*/) {
-      exitRender()
-      System.exit(0)
+  }
+}
+abstract class ScreenApp(unit_name:String = "Scage App", properties:String)
+extends Scage(unit_name, true, properties) with Renderer with ScageController with App {
+  override def run() {
+    init()
+    is_running = true
+    scage_log.info(unit_name+": run")
+    while(is_running && Scage.isAppRunning) {
+      checkControls()
+      for((action_id, action_operation, is_action_pausable) <- actions) {
+        current_operation_id = action_id
+        if(!on_pause || !is_action_pausable) action_operation()
+      }
+      render()
     }
+    exit()
+    dispose()
+    scage_log.info(unit_name+" was stopped")
+    exitRender()
+    System.exit(0)
+  }
+
+  override def main(args: Array[String]) {
+    super.main(args)
+    run()
   }
 }
 
-class ScageScreen(unit_name:String = "Scage App", is_main_unit:Boolean = false, properties:String = "")
-extends Screen(unit_name, is_main_unit, properties) with SingleController
+class ScageScreen(unit_name:String = "Scage Screen") extends Screen(unit_name) with SingleController
 
-class MultiControlledScreen(unit_name:String = "Scage App", is_main_unit:Boolean = false, properties:String = "")
-extends Screen(unit_name, is_main_unit, properties) with MultiController
+class ScageScreenApp(unit_name:String = "Scage App", properties:String) 
+extends ScreenApp(unit_name, properties) with SingleController
+
+/*class MultiControlledScreen(unit_name:String = "Scage App", is_main_unit:Boolean = false, properties:String = "")
+extends Screen(unit_name, is_main_unit, properties) with MultiController*/
--- ScageMaven/src/main/scala/net/scage/support/ScageColorTest.scala
+++ ScageMaven/src/main/scala/net/scage/support/ScageColorTest.scala
@@ -1,12 +1,12 @@
 package net.scage.support
 
+import _root_.net.scage.{ScageScreenApp, ScageScreen}
 import org.lwjgl.input.Keyboard._
 import _root_.net.scage.support.ScageColors._
-import _root_.net.scage.ScageScreen
 import _root_.net.scage.handlers.Renderer._
 import messages.ScageMessage._
 
-object ScageColorTest {
+object ScageColorTest extends ScageScreenApp("Color Test", properties="colortest-properties.txt") {
   val fields = ScageColors.getClass.getDeclaredFields
   val colors = fields.map(f => {
     f.setAccessible(true)
@@ -17,38 +17,34 @@ object ScageColorTest {
   })
 
   var color_num = 1
-  val main_screen = new ScageScreen("Color Test", is_main_unit = true, properties="colortest-properties.txt") {
-    interface {
-      if(color_num >= 0 && color_num < fields.length) {
-        print(colors(color_num), 20, window_height/2,
-          if("BLACK".equalsIgnoreCase(fields(color_num).getName)) WHITE else BLACK)
-        try {backgroundColor = (colors(color_num))}
-        catch {
-          case ex:java.lang.Exception =>
-        }
+  interface {
+    if(color_num >= 0 && color_num < fields.length) {
+      print(colors(color_num), 20, window_height/2,
+        if("BLACK".equalsIgnoreCase(fields(color_num).getName)) WHITE else BLACK)
+      try {backgroundColor = (colors(color_num))}
+      catch {
+        case ex:java.lang.Exception =>
       }
     }
-
-    key(KEY_LEFT, onKeyDown = {
-      def nextColorNumInc() {
-        if(color_num < fields.length - 1) color_num += 1
-        else color_num = 0
-        if(colors(color_num) != null && (!WHITE.equals(colors(color_num)) || "WHITE".equalsIgnoreCase(fields(color_num).getName))) color_num
-        else nextColorNumInc()
-      }
-      nextColorNumInc()
-    })
-    key(KEY_RIGHT, onKeyDown = {
-      def nextColorNumDec() {
-        if(color_num > 0) color_num -= 1
-        else color_num = fields.length - 1
-        if(!WHITE.equals(colors(color_num)) || "WHITE".equalsIgnoreCase(fields(color_num).getName)) color_num
-        else nextColorNumDec()
-      }
-      nextColorNumDec()
-    })
-    key(KEY_ESCAPE, onKeyDown = stop())
   }
 
-  def main(args:Array[String]) {main_screen.run()}
+  key(KEY_LEFT, onKeyDown = {
+    def nextColorNumInc() {
+      if(color_num < fields.length - 1) color_num += 1
+      else color_num = 0
+      if(colors(color_num) != null && (!WHITE.equals(colors(color_num)) || "WHITE".equalsIgnoreCase(fields(color_num).getName))) color_num
+      else nextColorNumInc()
+    }
+    nextColorNumInc()
+  })
+  key(KEY_RIGHT, onKeyDown = {
+    def nextColorNumDec() {
+      if(color_num > 0) color_num -= 1
+      else color_num = fields.length - 1
+      if(!WHITE.equals(colors(color_num)) || "WHITE".equalsIgnoreCase(fields(color_num).getName)) color_num
+      else nextColorNumDec()
+    }
+    nextColorNumDec()
+  })
+  key(KEY_ESCAPE, onKeyDown = stop())
 }
